1ecbc2d3f1309a9fd3834873b3628f579ded7a01
yousufhossainsiam <yousufhossainsiam@gmail.com>
2024-11-17 16:28:47 +0000

    Initial Commit

---
+++ lib/model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ModelMessage {
+  final bool isPrompt;
+  final String message;
+  final DateTime time;
+
+  ModelMessage({required this.isPrompt, 
+  required this.message, 
+  required this.time});
+
+}
